9e802b378695159e62acd70051e1f9da796dfd49
jaakkosy <jaakkosyrjamaki92@hotmail.com>
2022-06-08 21:12:57 +0300

    added robust and comments

--- ShoppingList/Program.cs
+++ ShoppingList/Program.cs
@@ -11,22 +11,45 @@ namespace ShoppingList
         {
             string filePath = @"C:\Users\JaakkoSyrjämäki\source\repos\ShoppingList\shoppinglist.txt";
             List<string> lines = File.ReadAllLines(filePath).ToList();
+            PrintShoppingList(lines);
+            CollectShoppingList(filePath,lines);
+            PrintShoppingList(lines);
+        }
 
+        // printing shopping list contents
+        public static List<string> PrintShoppingList(List<string> lines)
+        {
             foreach (string line in lines)
             {
                 Console.WriteLine(line);
             }
+            return lines;
+        }
+
+        // collecting shopping list contents from user
+        public static List<string> CollectShoppingList(string filePath,List<string> lines)
+        {
             while (true)
             {
 
-                Console.WriteLine("Write the product in the shoppinglist, to exit, write exit");
-                string userInput = Console.ReadLine();
+                Console.WriteLine("Write the product in the shoppinglist, to exit, write 'exit'");
+                string userInput;
+                try
+                {
+                    userInput = Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Enter input in correct form");
+                    continue;
+                }
                 if (userInput.ToLower() == "exit")
                 {
-                    break;
+                    return lines;
                 }
                 lines.Add(userInput);
                 File.WriteAllLines(filePath, lines);
+                continue;
             }
         }
     }
