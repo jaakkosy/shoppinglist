11528f936037ff875535f4c3a0a558186a2c7db5
jaakkosy <jaakkosyrjamaki92@hotmail.com>
2022-06-08 10:49:41 +0300

    made shoppinglist project

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\JaakkoSyrjämäki\source\repos\ShoppingList\shoppinglist.txt";
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Write the product in the shoppinglist:");
+            string userInput = Console.ReadLine();
+            lines.Add(userInput);
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
